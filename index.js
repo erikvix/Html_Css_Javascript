f005c5d4444ff5acb34fb6e71fda6f45e390ffd9
erikvix <erikvixx@gmail.com>
2023-03-07 10:56:31 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let x = "Car";
+let y = "Bike";
+let z = "Bus";
+
+console.log("I love my",x, "but i also love my", y, "and i cant stand my", z);
+document.getElementById("p1").innerHTML = "I love my " + x; 
+document.getElementById("p2").innerHTML = "but i also love my " + y;
+document.getElementById("p3").innerHTML = "and i cant stand my " + z;
