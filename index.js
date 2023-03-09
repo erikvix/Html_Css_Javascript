1189b7325b88035cf9a843a7e86b1a66ee915709
erikvix <erikvixx@gmail.com>
2023-03-09 06:41:17 -0300

    buttons_and_input

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-let x = "Car";
-let y = "Bike";
-let z = "Bus";
+let username;
 
-console.log("I love my",x, "but i also love my", y, "and i cant stand my", z);
-document.getElementById("p1").innerHTML = "I love my " + x; 
-document.getElementById("p2").innerHTML = "but i also love my " + y;
-document.getElementById("p3").innerHTML = "and i cant stand my " + z;
+document.getElementById("button").onclick = function(){
+    username = document.getElementById("input").value;
+    document.getElementById("label").innerHTML ="your age is " + username
+
+}
+
+ 
