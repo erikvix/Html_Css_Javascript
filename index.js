609082add89cee39856bae2daa66b6f48385bba3
erikvix <erikvixx@gmail.com>
2023-03-10 07:35:46 -0300

    basal_metabolism_rate

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-let username;
+let altura;
+let peso;
+let idade;
+let taxa;
 
-document.getElementById("button").onclick = function(){
-    username = document.getElementById("input").value;
-    document.getElementById("label").innerHTML ="your age is " + username
+taxa = peso / (altura * altura)
 
-}
 
- 
+ document.getElementById("button").onclick = function(){
+    peso = document.getElementById("weight").value;
+    peso = Number(peso);
+
+    altura = document.getElementById("height").value;
+    altura = Number(altura);
+
+    idade = document.getElementById("age").value;
+    idade = Number(idade);
+
+    document.getElementById("answer").innerHTML = "Seu metabolismo basal é  " + taxa    
+    console.log(Math.round(taxa))
+
+
+    taxa = 665 + (13.7 * peso) + (5.0 * altura) - (6.8 * idade)
+
+
+ }
