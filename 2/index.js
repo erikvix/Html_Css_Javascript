90c00967855c60a9ec94c5f3692abda6d951cc91
erikvix <erikvixx@gmail.com>
2023-03-22 11:34:28 -0300

    nothing

---
+++ 2/index.js
@@ -0,0 +1,19 @@
+let grade = "d"
+
+switch (grade){
+    case "a":
+        console.log("sim")
+        break
+    case "b":
+        console.log("nao")
+        break
+    case "c":
+        console.log("xd")
+        break
+    case "d":
+        console.log("wow")
+        break
+    case "e":
+        console.log("sss")
+        break
+}
